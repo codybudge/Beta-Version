426be7109a41a5e7026678e883ac0f56cf4424fc
GabeMcClaran <gabemcclaran@gmail.com>
2020-03-30 16:08:20 -0600

    fixed some routes, need more info on auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Home from "../views/Home.vue";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: Dashboard
-  }
-];
-
-export default router;
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      component: Dashboard
+    },
+    {
+      path: "*",
+      redirect: "/"
+    }
+  ]
+});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,33 @@
-<template></template>
+<template>
+  <div id="backboard">
+    <notifications group="login" />
+    <div id="login">
+      <form @submit.prevent="loginUser">
+        <label for="input1">Email</label>
+        <input id="input1" type="text" v-model="creds.email" required />
+        <label for="input2">Password</label>
+        <input id="input2" type="password" v-model="creds.password" required />
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account?
+        <!-- <router-link to="/sign_up" tag="h6" id="sign-up">Create One</router-link> -->
+      </p>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
   name: "Login",
+  data() {
+    return {
+      creds: {
+        email: "",
+        password: ""
+      }
+    };
+  },
   components: {}
 };
 </script>
