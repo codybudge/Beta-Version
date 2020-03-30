56c5e6c94b09c5abd50e773aa1fb09a1c2fda6cf
JohnBonde <jjbonde1@gmail.com>
2020-03-30 15:50:05 -0600

    router fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Home from "../views/Home.vue";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: Dashboard
-  }
-];
-
-export default router;
+Vue.use(Router);
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      component: Dashboard
+    }
+  ]
+});
