8c9860c2ac2fc98d959fa5a6100ebc276ca67b0d
GabeMcClaran <gabemcclaran@gmail.com>
2020-03-30 18:06:13 -0600

    lookin more like wire frame

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,63 @@
 <template>
-  <div id="backboard">
-    <notifications group="login" />
-    <div id="login">
-      <form @submit.prevent="loginUser">
-        <label for="input1">Email</label>
-        <input id="input1" type="text" v-model="creds.email" required />
-        <label for="input2">Password</label>
-        <input id="input2" type="password" v-model="creds.password" required />
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Don't have an account?
-        <!-- <router-link to="/sign_up" tag="h6" id="sign-up">Create One</router-link> -->
-      </p>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12" id="createForm">
+        <div id="backboard">
+          <notifications group="login" />
+          <div class="text-center" id="login">
+            <form @submit.prevent="loginUser">
+              <!-- <label for="input1">Email</label> -->
+              <div class="form-group center">
+                <input
+                  class="form-control"
+                  id="inputAddress"
+                  type="text"
+                  placeholder="Username"
+                  required
+                />
+              </div>
+              <!-- <label for="input2">Password</label> -->
+              <div class="form-group">
+                <input
+                  class="form-control"
+                  id="input2"
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <input class="form-control" id="input3" type="text" placeholder="Email" required />
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <input
+                    class="form-control"
+                    id="input4"
+                    type="text"
+                    placeholder="First Name"
+                    required
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <input
+                    class="form-control"
+                    id="input5"
+                    type="text"
+                    placeholder="Last Name"
+                    required
+                  />
+                </div>
+              </div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+          <p>
+            Don't have an account?
+            <!-- <router-link to="/sign_up" tag="h6" id="sign-up">Create One</router-link> -->
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,4 +78,8 @@ export default {
 </script>
 
 <style>
+#createForm {
+  display: flex;
+  justify-content: center;
+}
 </style>
